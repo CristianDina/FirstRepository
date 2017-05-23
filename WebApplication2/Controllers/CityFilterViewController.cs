dc731bb11e270d48a6443724d7accae2ec51d75d
RexFeral <cristi.dina93@yahoo.com>
2017-05-23 20:38:04 +0300

    lab7

---
+++ WebApplication2/Controllers/CityFilterViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CityFilterViewController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: CityFilterView
+        public ActionResult Index(CityFilterViewModel filterView)
+        {
+            IQueryable<City> query = dbContext.Cities;
+            if (filterView.Name != null)
+            {
+                query = query.Where(u => u.ApplicationUser.UserName.Contains(filterView.Name));
+                filterView.Results = query.ToList();
+            }
+            if (filterView.Email != null)
+            {
+                query = query.Where(u => u.ApplicationUser.Email.Contains(filterView.Email));
+                filterView.Results = query.ToList();
+            }
+            if (filterView.MinTroupCount != null)
+            {
+                query = query.Where(u => u.Troops.Sum(s => s.TroopCount)>filterView.MinTroupCount);
+                filterView.Results = query.ToList();
+            }
+            if (filterView.MaxTroupCount != null)
+            {
+                query = query.Where(u => u.Troops.Sum(s => s.TroopCount) < filterView.MaxTroupCount);
+                filterView.Results = query.ToList();
+            }
+
+            return View(filterView);
+        }
+    }
+}
--- WebApplication2/Models/City.cs
+++ WebApplication2/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,7 @@ namespace WebApplication2.Models
         public virtual IList<Mine> Mines { get; set; }
         public virtual IList<Resource> Resources { get; set; }
         public virtual IList<Building> Buildings { get; set; }
+        public virtual IList<Troop> Troops { get; set; }
 
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
@@ -87,6 +89,43 @@ namespace WebApplication2.Models
         public string Action { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+    }
+
+    public class Troop
+    {
+        public int TroopId { get; set; }
+        public int TroopTypeId { get; set; }
+        public virtual TroopType TroupType { get; set; }
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+        public int TroopCount { get; set; }
+
+    }
+
+
+    public class TroopType
+    {
+        public int TroopTypeId { get; set; }
+        [Required]
+        [StringLength(15)]
+        [MinLength(5)]
+        [RegularExpression("[A-z]*")]
+        public string Name { get; set; }
+        [Range(0,100)]
+        public double Attack { get; set; }
+        [Range(0, 100)]
+        public double Defence { get; set; }
+        [Range(0, 100)]
+        public int CreationSpeed { get; set; }
+    }
+    public class CityFilterViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int? MinTroupCount { get; set; }
+        public int? MaxTroupCount { get; set; }
+
+        public List<City> Results { get; set; }
     }
 
     public enum ResourceType
